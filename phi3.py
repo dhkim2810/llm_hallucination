54e40e69769f9ebfbdb248ed7adc49e86af352c5
DonghoonKim <dhkim2810@khu.ac.kr>
2024-05-19 23:23:37 -0400

    Add quantization model configuration

--- phi3.py
+++ phi3.py
@@ -1,13 +1,12 @@
 import os
 import random
+import pickle
 
 import torch
 from peft import (
-    PeftType,
     PromptTuningConfig,
     PromptTuningInit,
     TaskType,
-    get_peft_config,
     get_peft_model,
 )
 from torch.utils.data import DataLoader
@@ -15,6 +14,7 @@ from tqdm import tqdm
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
+    BitsAndBytesConfig,
     default_data_collator,
     get_linear_schedule_with_warmup,
 )
@@ -26,15 +26,16 @@ BASE_DIR = os.environ.get("SLRUM_TMPDIR", "tmp")
 LR = 1e-4
 BATCH_SIZE = 8
 NUM_EPOCHS = 50
+SEQ_LENGTH = 512
 SOFT_PROMPT_LENGTH = 64
 DEVICE = "cuda"
 
 MODEL = "microsoft/Phi-3-mini-4k-instruct"
 
 
-def create_item(row):
-    question = row["question"]
-    support = row["support"]
+def create_item(row, support=False):
+    question = f'Question: {row["question"]}'
+    support = f'Context : {row["support"]}'
     answer_indexs = ["A", "B", "C", "D"]
     choices = [
         row["distractor1"],
@@ -44,44 +45,48 @@ def create_item(row):
     ]
     choices = sorted(choices, key=lambda k: random.random())
     answer_idx = choices.index(row["correct_answer"])
-    answer = answer_indexs[answer_idx]  # + ". " + row['correct_answer']
+    answer = "Choose the correct alphabet of the answer from the following.\n" + "\n".join([f"{answer_indexs[i]}: {choice}" for i, choice in enumerate(choices)])
+
+    inputs = (
+        "\n".join([question, answer])
+        if not support
+        else "\n".join([question, support, answer])
+    )
+    targets = answer_indexs[answer_idx]
 
     return {
-        "sentence": f"Question: {question}",
-        "answers": "Choose the correct alphabet of the answer from the following.\n"
-        + "\n".join(
-            [f"{answer_indexs[i]}: {choice}" for i, choice in enumerate(choices)]
-        ),
-        "label": answer_idx,
-        "text_label": answer,
-        "support": f"Context: {support}",
+        "inputs" : inputs,
+        "targets" : targets,
     }
 
-
-def preprocess_function(data, tokenizer, support, max_length):
-    inputs = (
-        data["sentence"] + "\n" + data["answers"]
-        if not support
-        else data["sentence"] + "\n" + data["support"] + "\n" + data["answers"]
-    )
-    targets = data["text_label"]
-    model_inputs = tokenizer(
-        inputs,
-        max_length=max_length,
-        padding="max_length",
-        truncation=True,
-        return_tensors="pt",
-    )
-    labels = tokenizer(
-        targets,
-        max_length=2,
-        padding="max_length",
-        truncation=True,
-        return_tensors="pt",
-    )
-    labels = labels["input_ids"]
-    labels[labels == tokenizer.pad_token_id] = -100
-    model_inputs["labels"] = labels
+def preprocess_function(data, tokenizer, max_length):
+    batch_size = len(data["inputs"])
+    inputs = [f"Inputs : {x} Answer: " for x in data["inputs"]]
+    targets = [x for x in data["targets"]]
+    model_inputs = tokenizer(inputs)
+    labels = tokenizer(targets)
+    for i in range(batch_size):
+        sample_input_ids = model_inputs["input_ids"][i]
+        label_input_ids = labels["input_ids"][i] + [tokenizer.pad_token_id]
+        # print(i, sample_input_ids, label_input_ids)
+        model_inputs["input_ids"][i] = sample_input_ids + label_input_ids
+        labels["input_ids"][i] = [-100] * len(sample_input_ids) + label_input_ids
+        model_inputs["attention_mask"][i] = [1] * len(model_inputs["input_ids"][i])
+    # print(model_inputs)
+    for i in range(batch_size):
+        sample_input_ids = model_inputs["input_ids"][i]
+        label_input_ids = labels["input_ids"][i]
+        model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
+            max_length - len(sample_input_ids)
+        ) + sample_input_ids
+        model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
+            "attention_mask"
+        ][i]
+        labels["input_ids"][i] = [-100] * (max_length - len(sample_input_ids)) + label_input_ids
+        model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
+        model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
+        labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
+    model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
 
@@ -93,7 +98,7 @@ dataset = dataset.map(
 )
 
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
+tokenizer = AutoTokenizer.from_pretrained(MODEL, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.unk_token
 tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
 tokenizer.padding_side = "right"
@@ -102,10 +107,9 @@ processed_datasets = dataset.map(
     preprocess_function,
     fn_kwargs={
         "tokenizer": tokenizer,
-        "support": False,
-        "max_length": 512,
+        "max_length": SEQ_LENGTH,
     },
-    batched=False,
+    batched=True,
     num_proc=8,
     remove_columns=dataset["train"].column_names,
     load_from_cache_file=False,
@@ -138,9 +142,33 @@ peft_config = PromptTuningConfig(
     tokenizer_name_or_path=MODEL,
 )
 
-model = AutoModelForCausalLM.from_pretrained(MODEL)
+# Quantization and Model Configuration
+compute_dtype = getattr(torch, "float16")
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=False
+)
+
+# Load pre-trained model
+model_kwargs = {
+    "use_cache": False,
+    "trust_remote_code": True,
+    "attn_implementation": "flash_attention_2",
+    "quantization_config": quant_config,
+    "device_map": {"": 0},
+}
+
+model = AutoModelForCausalLM.from_pretrained(MODEL, **model_kwargs)
 model = get_peft_model(model, peft_config)
 print(model.print_trainable_parameters())
+
+
+# configuration = Phi3Config.from_pretrained(MODEL, trust_remote_code=True)
+# model = Phi3Model(configuration)
+# model = get_peft_model(model, peft_config)
+# print(model.print_trainable_parameters())
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
 lr_scheduler = get_linear_schedule_with_warmup(
@@ -151,6 +179,8 @@ lr_scheduler = get_linear_schedule_with_warmup(
 
 
 model = model.to(DEVICE)
+
+losses = {'train': [], 'val': []}
 
 for epoch in range(NUM_EPOCHS):
     model.train()
@@ -187,5 +217,10 @@ for epoch in range(NUM_EPOCHS):
     train_ppl = torch.exp(train_epoch_loss)
     print(f"{epoch=}: {train_ppl=} {train_epoch_loss=} {eval_ppl=} {eval_epoch_loss=}")
 
+    losses['train'].append((train_epoch_loss, train_ppl))
+    losses['val'].append((eval_epoch_loss, eval_ppl))
+
 # Save model
 model.save_pretrained(os.path.join(BASE_DIR, "ckpt"))
+with open(os.path.join(BASE_DIR, "result.pkl"), "wb") as f:
+    pickle.dump(losses, f)
