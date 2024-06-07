12e5bf30b8143c85f0ae99357c11f0f458dae35c
Donghoon Kim <dhkim2810@gmail.com>
2024-06-07 18:28:34 -0400

    Add accuracy calculation function

---
+++ utils.py
@@ -0,0 +1,11 @@
+def calc_accuracy(outputs) -> float:
+    count = 0
+    total = 0
+    for target, pred in zip(outputs["target"], outputs["preds"]):
+        target = target[0]
+        pred = pred[0]
+
+        total += 1
+        if input == pred:
+            count += 1
+    return count / total
