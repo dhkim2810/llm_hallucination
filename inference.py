ec984bb1071477673631fd45bcaa79c1fc422ba6
Donghoon Kim <dhkim2810@gmail.com>
2024-06-12 02:55:18 -0400

    Update inference code

--- inference.py
+++ inference.py
@@ -22,14 +22,9 @@ MODEL_DICT = {
 def generate(model, loader, tokenizer, device="cuda"):
     model_outputs = {"targets": [], "preds": []}
     for batch in tqdm.tqdm(loader):
-        batch = {
-            k: v.to(device) for k, v in batch.items()
-        }  # keys : inputs, attention_mask, labels
-
-        output = model.generate(**batch)
-        model_outputs["targets"].extend(
-            tokenizer.batch_decode(output, skip_special_tokens=True)
-        )
+        inputs = batch["inputs"].to(device)
+        output = model.generate(inputs)
+        model_outputs["targets"].extend(batch["answer"])
         model_outputs["preds"].extend(
             tokenizer.batch_decode(output, skip_special_tokens=True)
         )
@@ -39,15 +34,14 @@ def generate(model, loader, tokenizer, device="cuda"):
 def main(args):
     tokenizer = AutoTokenizer.from_pretrained(MODEL_DICT[args.model_name])
     tokenizer.pad_token = tokenizer.unk_token
-    tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
 
     # Load dataset
     val_dset = load_dataset(
         args.dataset,
-        split="validation",
+        split="generation",
         tokenizer=tokenizer,
-        max_seq_len=args.max_seq_len,
         format=args.model_name,
+        max_length=args.max_length,
         use_hint=args.use_hint,
     )
     val_loader = DataLoader(val_dset, batch_size=args.batch_size, shuffle=False)
@@ -72,9 +66,9 @@ def main(args):
     )
 
     with torch.no_grad():
-        loss, outputs = generate(model, val_loader, tokenizer, device="cuda")
+        outputs = generate(model, val_loader, tokenizer, device="cuda")
         accuracy = calc_accuracy(outputs)
-    print(f"[Validation]\n- Loss: {loss}\n- Accuracy: {accuracy}")
+    print(f"[Validation] Accuracy: {accuracy}")
 
     # Save outputs
     output_dir = f"{args.output_dir}/{args.model_name}_{args.dataset}"
@@ -82,11 +76,10 @@ def main(args):
         output_dir += "_hint"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
-    with open(f"{output_dir}/baseline.pkl", "wb") as f:
+    with open(f"{output_dir}/generation.pkl", "wb") as f:
         pkl.dump(
             {
                 "predictions": outputs,
-                "loss": loss,
                 "accuracy": accuracy,
             },
             f,
@@ -112,15 +105,10 @@ def parse_args(args=None):
         type=str,
         choices=["sciq", "scienceqa"],
     )
-    parser.add_argument("--max_seq_len", type=int, default=512)
-    parser.add_argument("--soft_prompt_length", type=int, default=64)
-
-    # Training/Validation arguments
-    parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--batch_size", type=int, default=8)
-    parser.add_argument("--num_epochs", type=int, default=50)
+    parser.add_argument("--max_length", type=int, default=256)
 
     # Inferencing arguments
+    parser.add_argument("--batch_size", type=int, default=8)
     parser.add_argument("--use_hint", action="store_true")
 
     args = parser.parse_args(args)
