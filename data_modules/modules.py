27079c28b30d1bcc366294424bb2e97d058e1ce3
Donghoon Kim <dhkim2810@gmail.com>
2024-06-12 02:50:11 -0400

    Update data modules for ScienceQA

--- data_modules/modules.py
+++ data_modules/modules.py
@@ -21,53 +21,108 @@ class Formatter:
     def __call__(self, **kwargs) -> dict[str, str]:
         return self.format_fn(**kwargs)
 
-    def _format_phi3(self, question, answer):
+    def _format_phi3(self, question, answer, is_generate=False):
         input = ""
         if self.system_prompt:
-            input += f"<|system|>\n{self.system_prompt}<|end|>\n"
-        input += f"<|user|>\n{question}<|end|>\n<|assistant|>\n"
+            input += f"<|user|> {self.system_prompt} {question}<|end|><|assistant|>"
+        else:
+            input += f"<|user|> {question}<|end|><|assistant|>"
         label = f"{answer})<|end|>"
-        input += label
+        if not is_generate:
+            input += label
         return {"input": input, "label": label, "answer": answer}
 
-    def _format_llama3(self, question, answer):
+    def _format_llama3(self, question, answer, is_generate=False):
         input = ""
         if self.system_prompt:
             input += f"<|begin_of_text|><|start_header_id|>system<|end_header_id|>]\n\n{self.system_prompt}<|eot_id|>"
-        input += f"<|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
-        label = f"\n\n{answer})<|eot_id|>"
-        input += label
+        input += f"<|start_header_id|>user<|end_header_id|>\n\n{question}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+        label = f"{answer})<|eot_id|>"
+        if not is_generate:
+            input += label
         return {"input": input, "label": label, "answer": answer}
 
 
-class SciQDataset(Dataset):
+class FineTuneDataset(Dataset):
     def __init__(
         self,
         tokenizer,
         split="train",
-        max_seq_len=512,
         format="phi3",
+        max_length=256,
+        use_system_prompt=True,
         use_hint=False,
     ):
-        assert split in [
-            "train",
-            "validation",
-            "test",
-        ], f"Invalid split provided : {split}."
-        self.split = split
+        self.split = "validation" if split == "gen" else split
         self.tokenizer = tokenizer
+        self.formatter = Formatter(format=format, system_prompt=use_system_prompt)
+        self.max_length = max_length
+        self.is_generation = split == "gen"
+        self.use_system_prompt = use_system_prompt
+        self.use_hint = use_hint
         self.padding_side = "right" if "llama3" in format else "left"
-        self.formatter = Formatter(format=format)
-        self.max_seq_len = max_seq_len
 
-        self.dataset = load_dataset("allenai/sciq", split=split)
+    def __len__(self) -> int:
+        pass
+
+    def __getitem__(self, idx):
+        pass
+
+    def _pad(self, tk_item: torch.Tensor) -> torch.Tensor:
+        return torch.nn.functional.pad(
+            tk_item,
+            (
+                (0, self.max_seq_len - tk_item.size(1))
+                if self.padding_side == "right"
+                else (self.max_seq_len - tk_item.size(1), 0)
+            ),
+            value=self.tokenizer.pad_token_id,
+        )
+
+    def _tokenize_item(self, question, answer) -> dict[str, torch.Tensor | int]:
+        tk_question = self.tokenizer(
+            question,
+            return_tensors="pt",
+            truncation=True,
+            max_length=self.max_length,
+        )
+        tk_answer = self.tokenizer(
+            answer,
+            return_tensors="pt",
+            truncation=True,
+            max_length=self.max_length,
+        )
+
+        tk_answer = tk_answer["input_ids"][:, 1:]  # truncate statr_of_text token
+        label = torch.ones_like(tk_question["input_ids"]) * -100
+        label[:, -tk_answer.size(1) :] = tk_answer
+
+        # append padding to max_seq_len
+        for key in tk_question:
+            tk_question[key] = self._pad(tk_question[key])
+        label = self._pad(label)
+
+        return {
+            "input_ids": tk_question["input_ids"].squeeze(),
+            "attention_mask": tk_question["attention_mask"].squeeze(),
+            "labels": label.squeeze(),
+        }
+
+
+class SciQDataset(FineTuneDataset):
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.dataset = load_dataset("allenai/sciq", split=self.split)
         self.dataset = self.dataset.map(
             self._create_sciq_item,
-            fn_kwargs={"hint": use_hint},
+            fn_kwargs={"hint": self.use_hint},
             batched=False,
             remove_columns=self.dataset.column_names,
             load_from_cache_file=False,
-        ).with_format("torch")
+        )
 
     def __len__(self) -> int:
         return len(self.dataset)
@@ -75,10 +130,26 @@ class SciQDataset(Dataset):
     def __getitem__(self, idx):
         return self.dataset[idx]
 
-    def _create_sciq_item(self, row, hint=False) -> dict[str, torch.Tensor | int]:
-        # question = f'Choose the correct alphabet of the answer from the following. Your answer should follow format of <alphabet>: <choice>.\nQuestion: {row["question"]}'
-        system = "You are the smartest student in class. Try your best answer correctly to questions."
-        question = f'The following are multiple choice questions about science. \n **Question** {row["question"]}'
+    def _create_sciq_item(self, example, hint=False) -> dict[str, torch.Tensor | int]:
+        # parse item
+        question, question_with_answer, answer = self._parse_row(example, hint)
+
+        # create message
+        if self.is_generation:
+            item = self.formatter(question=question, answer=answer, is_generate=True)
+            tk_item = self.tokenizer(
+                item["input"],
+                return_tensors="pt",
+                truncation=True,
+                max_length=self.max_length,
+            )["input_ids"].squeeze()
+            return {"inputs": tk_item, "answer": answer}
+        else:
+            item = self.formatter(question=question, answer=answer)
+            return self._tokenize_item(question, answer)
+
+    def _parse_row(self, row, hint=False) -> tuple[str, str]:
+        question = f'The following are multiple choice questions about science.\n**Question** {row["question"]}'
         if hint:
             question += f'\n **Hint** {row["support"]}'
         answer_indexs = ["A", "B", "C", "D"]
@@ -93,88 +164,92 @@ class SciQDataset(Dataset):
         choices = "\n".join(
             [f"({answer_indexs[i]}) {choice}" for i, choice in enumerate(choices)]
         )
-        question += f"\n **Choices** \n{choices} \n **Answer:** ("
+        question += f"\n**Choices** \n{choices}\n**Answer:** ("
+        question_with_answer = question + answer_indexs[answer_idx] + ")"
         answer = answer_indexs[answer_idx]
 
-        item = self.formatter(
-            question=question,
-            answer=answer,
-        )
-
-        tk_item = self._tokenize_item(item)
-
-        return tk_item
-
-    def _tokenize_item(self, item) -> dict[str, torch.Tensor | int]:
-        tk_question = self.tokenizer(
-            item["input"],
-            return_tensors="pt",
-        )
-        tk_answer = self.tokenizer(
-            item["label"],
-            return_tensors="pt",
-        )
-
-        tk_answer = tk_answer["input_ids"][:, 1:]  # truncate statr_of_text token
-        label = torch.ones_like(tk_question["input_ids"]) * -100
-        label[:, -tk_answer.size(1) :] = tk_answer
-
-        # append padding to max_seq_len
-        for key in tk_question:
-            tk_question[key] = torch.nn.functional.pad(
-                tk_question[key],
-                (
-                    (0, self.max_seq_len - tk_question[key].size(1))
-                    if self.padding_side == "right"
-                    else (self.max_seq_len - tk_question[key].size(1), 0)
-                ),
-                value=self.tokenizer.pad_token_id,
-            )
-        label = torch.nn.functional.pad(
-            label,
-            (
-                (0, self.max_seq_len - label.size(1))
-                if self.padding_side == "right"
-                else (self.max_seq_len - label.size(1), 0)
-            ),
-            value=-100,
-        )
-
-        return {
-            "input_ids": tk_question["input_ids"].squeeze(),
-            "attention_mask": tk_question["attention_mask"].squeeze(),
-            "labels": label.squeeze(),
-        }
+        return question, question_with_answer, answer
 
 
 class ScienceQADataset:
-    def __init__(self, df, shot_qids, args, shot_df, tokenizer):
-        super().__init__()
-        self.prompts = []
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.dataset = load_dataset("allenai/sciq", split=self.split)
+        self.dataset = self.dataset.filter(lambda x: x["image"] == None)
+        self.dataset = self.dataset.map(
+            self._create_scienceqa_item,
+            fn_kwargs={"hint": self.use_hint},
+            batched=False,
+            remove_columns=self.dataset.column_names,
+            load_from_cache_file=False,
+        )
 
-        shot_added_df = pd.concat((df, shot_df))
-        shot_added_dict = shot_added_df.transpose().to_dict()
+    def __len__(self) -> int:
+        return len(self.dataset)
 
-        for qid in tqdm([str(x) for x in df.index if x not in shot_qids]):
-            prompt = build_prompt(shot_added_dict, shot_qids, qid, args)
+    def __getitem__(self, idx):
+        return self.dataset[idx]
 
-            self.prompts.append(prompt)
-        self.answers = df.progress_apply(
-            lambda x: x["choices"][x["answer"]], axis=1
-        ).to_list()
-        self.distractors = df.progress_apply(
-            lambda x: ";".join(
-                [
-                    choice
-                    for choice in x["choices"]
-                    if choice is not x["choices"][x["answer"]]
-                ]
-            ),
-            axis=1,
-        ).to_list()
+    def _create_scienceqa_item(
+        self, example, hint=False
+    ) -> dict[str, torch.Tensor | int]:
+        # parse item
+        question, question_with_answer, answer = self._parse_row(example, hint)
 
-    def __len__(self):
-        return len(self.prompts)
+        # create message
+        if self.is_generation:
+            item = self.formatter(question=question, answer=answer, is_generate=True)
+            tk_item = self.tokenizer(
+                item["input"],
+                return_tensors="pt",
+                truncation=True,
+                max_length=self.max_length,
+            )["input_ids"].squeeze()
+            return {"inputs": tk_item, "answer": answer}
+        else:
+            item = self.formatter(question=question, answer=answer)
+            return self._tokenize_item(question, answer)
 
-    def __getitem__(self, index):
-        return self.prompts[index], self.answers[index], self.distractors[index]
+    def _parse_row(self, row, hint=False) -> tuple[str, str]:
+        context = self.get_context_text(row, use_caption=False)
+        question = self.get_question_text(row)
+        choice = self.get_choice_text(row)
+        answer = self.get_answer(row)
+
+        input = (
+            f"Context: {context}\nQuestion: {question}\nOptions: {choice}\n"
+            if hint
+            else f"Question: {question}\nOptions: {choice}\n"
+        )
+        answer = f"Answer: The answer is ("  # {answer}."
+        question = input + answer
+        question = question.replace("  ", " ").strip()
+        question_with_answer = question + f"{answer})"
+
+        return question, question_with_answer, answer
+
+    def get_question_text(self, problem):
+        question = problem["question"]
+        return question
+
+    def get_context_text(self, problem, use_caption):
+        txt_context = problem["hint"]
+        img_context = problem["caption"] if use_caption else ""
+        context = " ".join([txt_context, img_context]).strip()
+        if context == "":
+            context = "N/A"
+        return context
+
+    def get_choice_text(self, probelm, options=["A", "B", "C", "D", "E"]):
+        choices = probelm["choices"]
+        choice_list = []
+        for i, c in enumerate(choices):
+            choice_list.append("({}) {}".format(options[i], c))
+        choice_txt = " ".join(choice_list)
+        return choice_txt
+
+    def get_answer(self, problem, options=["A", "B", "C", "D", "E"]):
+        return options[problem["answer"]]
